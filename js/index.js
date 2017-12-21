dc37ec9dde29488539b7fa331bd263023f1b5115
xdd <xdd.com>
2017-12-21 18:30:58 +0800

    第一次提交

---
+++ js/index.js
@@ -0,0 +1,15 @@
+ $(function(){
+ 	var swiper = new Swiper('.swiper-container', {
+        spaceBetween: 10,
+        slidesPerView: 6,
+        centeredSlides: true,
+        slideToClickedSlide: true,
+        grabCursor: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+//      scrollbar: '.swiper-scrollbar',
+//      pagination: '.swiper-pagination',
+        history: 'slide',
+    });
+ })
+ 
